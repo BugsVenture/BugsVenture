c292f521f30d36ce6ed940c97e7a6b92af95c788
Unknown <K.schaefer92@gmx.de>
2018-05-07 10:57:11 +0200

    updated to unity 2018

--- Bugs Venture/Assets/Scripts/AI/EnemyBehavior.cs
+++ Bugs Venture/Assets/Scripts/AI/EnemyBehavior.cs
@@ -2,19 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Enemy))]
+[RequireComponent(typeof(IBaseEnemy))]
 public class EnemyBehavior : MonoBehaviour {
 
-    Enemy enemy; 
-	// Use this for initialization
-	void Start ()
+    IBaseEnemy enemy;
+
+    public float SigthAngle = 45;
+    public float ActivationDistance = 20;
+    public float AttackRange = 5;
+    public float fireRate = 5f;
+
+    // Use this for initialization
+    void Start ()
     {
-        enemy = GetComponent<Enemy>();
+        enemy = GetComponent<IBaseEnemy>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		//if(Vector3.Distance(enemy.transform.position, ))
-	}
+        if (Player.GetInstance())
+        {
+            if (Vector3.Distance(this.transform.position, Player.GetInstance().transform.position) < ActivationDistance)
+            {
+                Vector3 targetDir = Player.GetInstance().transform.position - this.transform.position;
+                if (Vector3.Angle(targetDir, transform.forward) < SigthAngle)
+                {
+                    if (Vector3.Distance(this.transform.position, Player.GetInstance().transform.position) < AttackRange)
+                    {
+                        StartCoroutine(enemy.Attack());
+                    }
+                    else
+                    {
+                        enemy.MoveTo(Player.GetInstance().transform.position, 1.0f);
+                    }
+                }
+            }
+        }
+    }
 }
