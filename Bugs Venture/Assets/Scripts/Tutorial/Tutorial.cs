f881a62bc60d1de0cedafc495efbcc555478b1c3
Almin <alminbibic@web.de>
2018-08-08 10:33:14 +0200

    add Tutorial

---
+++ Bugs Venture/Assets/Scripts/Tutorial/Tutorial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    //Public
+    public GameObject TutUi;
+
+    // Use this for initialization
+    void Start ()
+    {
+        TutUi.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Destroy(this.TutUi);
+        }
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            TutUi.SetActive(true);
+        }
+    }
+}
---
+++ Bugs Venture/Assets/Scripts/Tutorial/TutorialAbility.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialAbility : MonoBehaviour
+{
+
+    //Public
+    public GameObject TutUiAbility;
+
+    // Use this for initialization
+    void Start()
+    {
+        TutUiAbility.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(GameManager.GM.teleportKey)||
+           Input.GetKeyDown(KeyCode.Joystick1Button4))
+        { 
+            Destroy(this.TutUiAbility);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            TutUiAbility.SetActive(true);
+        }
+    }
+}
